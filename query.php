7e666b14a2c59acc0db453ea1315a1e192e5e48a
kiang <kiange@gmail.com>
2016-01-03 05:04:46 +0800

    divide the 2 days query into 2 hours

--- query.php
+++ query.php
@@ -1,12 +1,21 @@
 <?php
 
-$lastEndDate = mktime();
+$endDate = mktime();
 $lastResultCount = 1;
 $startDate = strtotime('-2 days');
 
 $basePath = __DIR__ . "/requests";
 
-while ($lastResultCount > 0 && $lastEndDate > $startDate) {
+if (false === getXml($startDate, $endDate)) {
+    while ($endDate > $startDate) {
+        $newStart = $endDate - 7200;
+        getXml($newStart, $endDate);
+        $endDate = $newStart;
+    }
+}
+
+function getXml($startDate, $endDate) {
+    global $basePath;
     $xmlWriter = new XMLWriter();
     $xmlWriter->openMemory();
     $xmlWriter->startDocument('1.0', 'UTF-8');
@@ -21,7 +30,7 @@ while ($lastResultCount > 0 && $lastEndDate > $startDate) {
     $xmlWriter->endElement();
 
     $xmlWriter->startElement('end_date');
-    $xmlWriter->writeCData(date('Y-m-d H:i:s', $lastEndDate));
+    $xmlWriter->writeCData(date('Y-m-d H:i:s', $endDate));
     $xmlWriter->endElement();
 
     $xmlWriter->endElement();
@@ -37,36 +46,35 @@ while ($lastResultCount > 0 && $lastEndDate > $startDate) {
     );
     $context = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-//    file_put_contents(__DIR__ . '/tmp/xml', $xml);
-//    file_put_contents(__DIR__ . '/tmp/result', $result);
-//    exit();
+    if (false !== strpos($result, '<returncode>0</returncode>')) {
+        $xml = simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $lastResultCount = (int) $xml->count;
 
-    $xml = simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA);
-    $lastResultCount = (int) $xml->count;
+        echo 'lastEndDate: ' . date('Y-m-d H:i:s', $endDate) . ', got ' . $lastResultCount . " records\n";
 
-    echo 'lastEndDate: ' . date('Y-m-d H:i:s', $lastEndDate) . ', got ' . $lastResultCount . " records\n";
-    
-    if ($lastResultCount > 0) {
-        foreach ($xml->records->record AS $record) {
-            $timeStamp = strtotime($record->requested_datetime);
-            if ($timeStamp < $lastEndDate) {
-                $lastEndDate = $timeStamp - 1;
-            }
-            $d = date('Y/m/d', $timeStamp);
-            $requestPath = "{$basePath}/{$d}";
-            if (!file_exists($requestPath)) {
-                mkdir($requestPath, 0777, true);
-            }
-            if (!empty($record->Pictures)) {
-                $picCount = 0;
-                foreach ($record->Pictures AS $pic) {
-                    ++$picCount;
-                    file_put_contents("{$requestPath}/{$record->service_request_id}-{$picCount}.jpg", base64_decode((string) $pic->Picture->file));
+        if ($lastResultCount > 0) {
+            foreach ($xml->records->record AS $record) {
+                $timeStamp = strtotime($record->requested_datetime);
+                if ($timeStamp < $endDate) {
+                    $endDate = $timeStamp - 1;
                 }
-                unset($record->Pictures);
+                $d = date('Y/m/d', $timeStamp);
+                $requestPath = "{$basePath}/{$d}";
+                if (!file_exists($requestPath)) {
+                    mkdir($requestPath, 0777, true);
+                }
+                if (!empty($record->Pictures)) {
+                    $picCount = 0;
+                    foreach ($record->Pictures AS $pic) {
+                        ++$picCount;
+                        file_put_contents("{$requestPath}/{$record->service_request_id}-{$picCount}.jpg", base64_decode((string) $pic->Picture->file));
+                    }
+                    unset($record->Pictures);
+                }
+                file_put_contents("{$requestPath}/{$record->service_request_id}.json", json_encode($record, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
             }
-            file_put_contents("{$requestPath}/{$record->service_request_id}.json", json_encode($record, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
+    } else {
+        return false;
     }
 }
-
